de735f1301502f7d1c137916b070cd02f2f4e96e
Walid <wawkadura@github.com>
2021-03-27 17:02:58 +0100

    feat: migration files (don't change the names)

---
+++ backend/database/migrations/2021_03_26_1003_create_adresses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('adresse_postale');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adresses');
+    }
+}
---
+++ backend/database/migrations/2021_03_26_1004_create_entreprises_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntreprisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entreprises', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nom');
+            $table->text('url')->nullable();
+            $table->binary('logo')->nullable();
+            $table->foreignId('adresse_id');
+
+            $table->foreign('adresse_id')
+              ->references('id')
+              ->on('adresses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entreprises');
+    }
+}

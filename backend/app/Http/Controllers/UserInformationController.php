36995842f432cf43f9d05b9759c5d3fcfe62978e
wilfrid beaunes <kesnelbeaunes@gmail.com>
2021-04-09 22:07:30 +0200

    page front in progress

---
+++ backend/app/Http/Controllers/UserInformationController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Adresse;
+use App\Models\Artisan;
+use App\Models\CategorieProfessionelle;
+use App\Models\Compte;
+use App\Models\Entreprise;
+use App\Models\Photo;
+use App\Models\Travaux;
+use Illuminate\Http\Request;
+
+
+class UserInformationController extends Controller
+{
+    // user connected
+    public function userConnected(Request $request){
+        $id= $request->input('id'); //id of the user connected 
+        
+        //now let use this value to get user information in database
+        $usercompte=Compte::where('id',$id)->firstOrFail();
+        $userartisan=Artisan::where('compte_id',$id)->firstOrFail();
+        $useradresse=Adresse::where('id',$id)->firstOrFail();
+        $userentreprise=Entreprise::where('adresse_id',$id)->firstOrFail();
+        $usertravaux=Travaux::where('artisan_id',$id)->firstOrFail();
+        $userphoto=Photo::where('travaux_id',$id)->firstOrFail();
+        $usercatpro=CategorieProfessionelle::where('id',$id)->firstOrFail();
+
+        $user = ["usercompte"=>$usercompte,"useradresse"=>$useradresse,
+        "userartisan"=>$userartisan,"userentreprise"=>$userentreprise,
+        "userphoto"=>$userphoto,"usertravaux"=>$usertravaux,"usercatpro"=>$usercatpro];
+        
+        return response()->json($user);
+
+    }
+}
